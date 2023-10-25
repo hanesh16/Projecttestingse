02b78a8c2492c0fc19c1bd33ec8ddebc4bac26a6
Hanesh <hanesh@pdx.edu>
2023-10-25 11:19:20 -0700

    Project Setup

---
+++ psueventsweb/events/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm
